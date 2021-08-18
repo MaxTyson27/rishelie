17d1f9c82208c5b508d0d0308909272ed3639d17
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2021-08-19 00:22:01 +0300

    added drop-down

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
+  $('.menu__list-link--info').on('click', function () {
+    $('.drop-down').toggleClass('drop-down--active');
+    $('.header__bg').toggleClass('active-drop-down');
+  });
+  $('.header__bg').on('click', function () {
+    $('.drop-down').removeClass('drop-down--active');
+    $(this).removeClass('active-drop-down');
+  });
 });
