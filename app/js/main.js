75a2d0cae47c270b38ac9a8c3583dbb054a46e62
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2021-08-19 17:38:30 +0300

    add 2 sections

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,73 @@ $(function () {
     $('.drop-down').removeClass('drop-down--active');
     $(this).removeClass('active-drop-down');
   });
+  $('.header__content-slider').slick({
+    fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+
+  $('.design__tabs-link').on('click', function (e) {
+    e.preventDefault();
+    $('.design__tabs-link').removeClass('design__tabs-link--active');
+    $(this).addClass('design__tabs-link--active');
+
+    $('.design__tabs-content-item').removeClass('design__tabs-content-item--active');
+    $($(this).attr('href')).addClass('design__tabs-content-item--active');
+  });
+
+
+  $('.design__tabs-gender-link--man, .bathrobe').on('click', function (e) {
+    e.preventDefault();
+    var manUrl = "images/design/man-bg.jpg";
+    $(".design").css("background-image", "url(" + manUrl + ")");
+  });
+
+
+
+  $('.design__tabs-gender-link--wooman').on('click', function (e) {
+    e.preventDefault();
+    var woomanUrl = "images/design/wooman-bg.jpg";
+    $(".design").css("background-image", "url(" + woomanUrl + ")");
+  });
+
+  $('.design__tabs-gender-link--child').on('click', function (e) {
+    e.preventDefault();
+    var childUrl = "images/design/child-bg.jpg";
+    $(".design").css("background-image", "url(" + childUrl + ")");
+  });
+
+
+
+  $('.design__tabs-size-link').on('click', function (e) {
+    e.preventDefault();
+    $('.design__tabs-size-link').removeClass('design__tabs-size-link--active');
+    $(this).addClass('design__tabs-size-link--active');
+  });
+
+  $('.design__tabs-color-link').on('click', function (e) {
+    e.preventDefault();
+    $('.design__tabs-color-link').removeClass('design__tabs-color-link--active');
+    $(this).addClass('design__tabs-color-link--active');
+  });
+
+  $('.design__tabs-gender-link').on('click', function (e) {
+    e.preventDefault();
+    $('.design__tabs-gender-link').removeClass('gender-link--active');
+    $(this).addClass('gender-link--active');
+  });
+
+  $('.towel').on('click', function () {
+    var towelUrl = "images/design/towel-bg.jpg";
+    $(".design").css("background-image", "url(" + towelUrl + ")");
+  });
+
+  $('.pillow').on('click', function () {
+    var pillowUrl = "images/design/pillow.jpg";
+    $(".design").css("background-image", "url(" + pillowUrl + ")");
+  });
+
+
 });
