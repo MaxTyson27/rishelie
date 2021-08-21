db0598fe53276fe783e0fd00599d07e92cb4260e
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2021-08-21 22:22:17 +0300

    added sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 $(function () {
   $('.menu__list-link--info').on('click', function (e) {
     e.preventDefault();
@@ -61,6 +65,19 @@ $(function () {
     navigation: {
       nextEl: '.swiper-button-next-catalog',
       prevEl: '.swiper-button-prev-catalog',
+    },
+  });
+
+  var swiperResult = new Swiper('.result__swiper', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    pagination: {
+      el: '.swiper-pagination-result',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-result',
+      prevEl: '.swiper-button-prev-result',
     },
   });
 
@@ -157,4 +174,134 @@ $(function () {
     $('#addPicture').removeClass('modal-pic--active');
   });
 
+  lightbox.option({
+    'showImageNumberLabel': false,
+  });
+
 });
+
+
+
+setTimeout((function () {
+  const e = document.documentElement,
+    o = document.getElementById("waterCanvas"),
+    t = o.getContext("2d"),
+    i = (e) => "video/hahaha_" + e.toString().padStart(5, "0") + ".webp",
+    s = new Image;
+  s.src = i(1), o.width = 1920, o.height = 1080, s.onload = function () { t.drawImage(s, 0, 0) };
+  const r = document.querySelector(".info").scrollHeight;
+  const n = document.querySelector(".header").scrollHeight + r;
+  window.addEventListener("scroll", () => {
+    if (n < e.scrollTop) {
+      const o = document.querySelector(".info");
+      const r = document.querySelector(".header");
+      const n = document.querySelector(".scroll-water");
+      const c = (e.scrollTop - r.scrollHeight - o.scrollHeight) / (n.scrollHeight - window.innerHeight);
+      const p = Math.min(129, Math.ceil(130 * c));
+      requestAnimationFrame(() => {
+        return e = p + 1, s.src = i(e), void t.drawImage(s, 0, 0);
+        var e
+      });
+    };
+  }), (() => {
+    for (let e = 1; e < 130; e++) {
+      (new Image).src = i(e)
+    }
+  })()
+}), 1e3), setTimeout((function () {
+  const e = document.documentElement,
+    o = document.getElementById("towelCanvas"),
+    t = o.getContext("2d"),
+    i = e => "videoTwo/hahaha_" + e.toString().padStart(5, "0") + ".webp",
+    s = new Image;
+  s.src = i(1), o.width = 1080, o.height = 1920, s.onload = function () {
+    t.drawImage(s, 0, 0)
+  };
+  const r = document.querySelector(".info").scrollHeight,
+    n = document.querySelector(".header").scrollHeight + r + document.querySelector(".scroll-water").scrollHeight;
+  window.addEventListener("scroll", () => {
+    if (n < e.scrollTop) {
+      const r = document.querySelector(".scroll-water"),
+        n = document.querySelector(".scroll-towel"),
+        c = document.querySelector(".info"),
+        p = document.querySelector(".header"),
+        l = (e.scrollTop - p.scrollHeight - c.scrollHeight - r.scrollHeight) / (n.scrollHeight - window.innerHeight),
+        a = Math.min(33, Math.ceil(34 * l));
+      Math.ceil(34 * l) > 147 && (o.style.position = "absolute"), Math.ceil(34 * l) < 148 && (o.style.position = "absolute"), requestAnimationFrame(() => {
+        return e = a + 1, s.src = i(e), void t.drawImage(s, 0, 0);
+        var e
+      })
+    }
+  }), (() => {
+    for (let e = 1; e < 34; e++) {
+      (new Image).src = i(e)
+    }
+  })()
+}), 3e3), setTimeout((function () {
+  const e = document.documentElement,
+    o = document.getElementById("threadCanvas"),
+    t = o.getContext("2d"),
+    i = e => "videoThree/hahaha_" + e.toString().padStart(5, "0") + ".webp",
+    s = new Image;
+  s.src = i(1), o.width = 1080, o.height = 1920, s.onload = function () {
+    t.drawImage(s, 0, 0)
+  };
+  const r = document.querySelector(".info").scrollHeight,
+    n = document.querySelector(".header").scrollHeight + r + document.querySelector(".scroll-water").scrollHeight + document.querySelector(".scroll-towel").scrollHeight;
+  window.addEventListener("scroll", () => {
+    if (n < e.scrollTop) {
+      const r = document.querySelector(".scroll-water"),
+        n = document.querySelector(".scroll-thread"),
+        c = document.querySelector(".scroll-towel"),
+        p = document.querySelector(".info"),
+        l = document.querySelector(".header"),
+        a = (e.scrollTop - l.scrollHeight - p.scrollHeight - r.scrollHeight - c.scrollHeight) / (n.scrollHeight - window.innerHeight),
+        u = Math.min(114, Math.ceil(115 * a));
+      Math.ceil(115 * a) > 147 && (o.style.position = "absolute"), Math.ceil(115 * a) < 148 && (o.style.position = "absolute"), requestAnimationFrame(() => {
+        return e = u + 1, s.src = i(e), void t.drawImage(s, 0, 0);
+        var e
+      })
+    }
+  }), (() => {
+    for (let e = 1; e < 115; e++) {
+      (new Image).src = i(e)
+    }
+  })()
+}), 4e3);
+
+
+
+// const html = document.documentElement;
+// const video1 = document.querySelector('#video1');
+// const context = video1.getContext('2d');
+// const headerInfoHeight = document.querySelector('.info').scrollHeight + document.querySelector('.header').scrollHeight;
+
+// const currentFrame = index => (
+//   `video/hahaha_${index.toString().padStart(5, "0")}.webp`
+// )
+
+// const frameCount = 130;
+
+// video1.height = 1080;
+// video1.width = 1920;
+// const img = new Image();
+// img.src = currentFrame(2);
+// img.onload = function () {
+//   context.drawImage(img, 0, 0);
+// }
+
+// const updateImage = index => {
+//   img.src = currentFrame(index);
+//   context.drawImage(img, 0, 0);
+// }
+
+// window.addEventListener('scroll', () => {
+//   const scrollTop = html.scrollTop;
+//   const maxScrollTop = html.scrollHeight - window.innerHeight;
+//   const scrollFraction = scrollTop / maxScrollTop;
+//   const frameIndex = Math.min(
+//     frameCount - 1,
+//     Math.floor(scrollFraction * frameCount)
+//   );
+//   requestAnimationFrame(() => updateImage(frameIndex + 1));
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/swiper/swiper-bundle.js',
+    'node_modules/lightbox2/dist/js/lightbox.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
